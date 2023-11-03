caba4bbc599fb0293269ea587e8d3b11b0b18d85
Gustanto <gustanto.mucharom@rschooltoday.com>
2023-11-03 12:25:58 +0700

    session, get and create user

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,9 +3,11 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import AuthProviders from "./AuthProviders";
+import { getCurrentUser } from "@/lib/session";
 
-const Navbar = () => {
-  const session = {};
+const Navbar = async () => {
+  const session = await getCurrentUser();
+
   return (
     <nav className="flexBetween nevbar">
       <div className="flex-1 flexStart gap-10">
@@ -22,15 +24,21 @@ const Navbar = () => {
       </div>
 
       <div className="flexCenter gap-4">
-        {session ? (
-            <>
-                UserPhoto
-                <Link href="/create-project">
-                    Share Work
-                </Link>
-            </>
+        {session?.user ? (
+          <>
+            {session?.user?.image && (
+              <Image
+                src={session.user.image}
+                width={40}
+                height={40}
+                className="rounded-full"
+                alt={session.user.name}
+              />
+            )}
+            <Link href="/create-project">Share Work</Link>
+          </>
         ) : (
-            <AuthProviders />
+          <AuthProviders />
         )}
       </div>
     </nav>
